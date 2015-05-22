2f9b5e66059339aa84ec41f1874162ba9783d2fc
Christopher Rienzo <chris.rienzo@citrix.com>
2015-05-22 08:56:50 -0400

    FS-7553 fix spec file - wrong dependency for devel package

--- build_rpm.sh
+++ build_rpm.sh
@@ -8,7 +8,7 @@ cd ..
 (mkdir -p rpmbuild && cd rpmbuild && mkdir -p SOURCES BUILD BUILDROOT i386 x86_64 SPECS)
 
 cp ${ORIGDIR}/*.spec rpmbuild/SPECS/.
-cp ${ORIGDIR}/g722_1-0.2.0.tar.gz rpmbuild/SOURCES/.
+cp ${ORIGDIR}/*.gz rpmbuild/SOURCES/.
 
 rpmbuild --define "_topdir %(pwd)/rpmbuild" \
   --define "_rpmdir %{_topdir}" \
